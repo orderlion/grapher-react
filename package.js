a198c0593dc67c5a0475bee3b083307cbde950c5
Patrick Schubert <patrick.schubert1@gmail.com>
2022-03-14 10:05:49 +0100

    Update package.js
    removed version restrictions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'cultofcoders:grapher-react',
-    version: '0.1.5',
+    version: '0.1.6',
     // Brief, one-line summary of the package.
     summary:
         'Provides HOCs for easily wrapping components with Grapher queries',
@@ -18,9 +18,9 @@ Package.onUse(function(api) {
         'tracker',
         'check',
         'reactive-var',
-        'react-meteor-data@2.1.0',
-        'cultofcoders:grapher@1.3.19',
-        'tmeasday:check-npm-versions@0.3.2',
+        'react-meteor-data',
+        'cultofcoders:grapher',
+        'tmeasday:check-npm-versions',
     ]);
 
     api.mainModule('main.client.js', 'client');
